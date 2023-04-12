454218313ba2556adbba3d3bcde0249104ef48b4
Donia Elshenawy <doniaelshenawyali@gmail.com>
2023-04-12 04:51:44 +0200

    add charity profile ui

---
+++ Khaier_App/View Controller/charity Profile/CharityProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CharityProfileViewController.swift
+//  Khaier_App
+//
+//  Created by Donia Elshenawy on 12/04/2023.
+//
+
+import UIKit
+
+class CharityProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
