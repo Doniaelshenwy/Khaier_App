8b20503381bd682a53453a08f4d51f3fa0add0f7
Donia Elshenawy <doniaelshenawyali@gmail.com>
2023-04-17 06:21:58 +0200

    add donation details screen ui

---
+++ Khaier_App/View Controller/Donation Details/DonatinDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DonatinDetailsViewController.swift
+//  Khaier_App
+//
+//  Created by Donia Elshenawy on 17/04/2023.
+//
+
+import UIKit
+
+class DonatinDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        isNavigationHidden(true)
+        isTabBarHidden(true)
+
+    }
+
+}
